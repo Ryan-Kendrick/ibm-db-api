2b5e956322d4f83d4b78dfec5ec28d84badca0fd
Ryan Kendrick <ryan.kendrick93@gmail.com>
2023-06-25 17:22:34 +1200

    add database interactions

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import ibm_db
 import json
+import pandas
+import ibm_db_dbi
 
 with open("credentials.json", "r") as credfile:
   credentials = json.load(credfile)
   dsn_hostname = credentials['hostname']
   dsn_uid = credentials['uid']
   dsn_pwd = credentials['pwd']
+
+# API setup
 
 dsn_driver = "{IBM DB2 ODBC DRIVER}"
 dsn_database = "BLUDB"            # e.g. "BLUDB"
@@ -32,16 +36,12 @@ try:
 except:
     print ("Unable to connect: ", ibm_db.conn_errormsg() )
 
-#Retrieve Metadata for the Database Server
 server = ibm_db.server_info(conn)
-
 print ("DBMS_NAME: ", server.DBMS_NAME)
 print ("DBMS_VER:  ", server.DBMS_VER)
 print ("DB_NAME:   ", server.DB_NAME)
 
-#Retrieve Metadata for the Database Client / Driver
 client = ibm_db.client_info(conn)
-
 print ("DRIVER_NAME:          ", client.DRIVER_NAME) 
 print ("DRIVER_VER:           ", client.DRIVER_VER)
 print ("DATA_SOURCE_NAME:     ", client.DATA_SOURCE_NAME)
@@ -50,5 +50,36 @@ print ("ODBC_VER:             ", client.ODBC_VER)
 print ("ODBC_SQL_CONFORMANCE: ", client.ODBC_SQL_CONFORMANCE)
 print ("APPL_CODEPAGE:        ", client.APPL_CODEPAGE)
 print ("CONN_CODEPAGE:        ", client.CONN_CODEPAGE)
+
+# DB interactions
+
+try:
+  dropQuery = "drop table INSTRUCTOR"
+  dropStmt = ibm_db.exec_immediate(conn, dropQuery)
+except:
+   print('Unable to drop table')
+finally:
+  createQuery = "create table INSTRUCTOR(ID INTEGER PRIMARY KEY NOT NULL, FNAME VARCHAR(20), LNAME VARCHAR(20), CITY VARCHAR(20), CCODE CHAR(2))"
+  createStmt = ibm_db.exec_immediate(conn, createQuery)
+
+insertQuery = "insert into INSTRUCTOR values (1, 'Rav', 'Ahuja', 'TORONTO', 'CA')"
+insertStmt = ibm_db.exec_immediate(conn, insertQuery)
+insertQuery2 = "insert into INSTRUCTOR values (2, 'Raul', 'Chong', 'Markham', 'CA'), (3, 'Hima', 'Vasudevan', 'Chicago', 'US')"
+insertStmt2 = ibm_db.exec_immediate(conn, insertQuery2)
+
+selectQuery = "select * from INSTRUCTOR"
+selectStmt = ibm_db.exec_immediate(conn, selectQuery)
+ibm_db.fetch_both(selectStmt)
+while ibm_db.fetch_row(selectStmt) != False:
+    print (" ID:",  ibm_db.result(selectStmt, 0), " FNAME:",  ibm_db.result(selectStmt, "FNAME"))
+
+# Pandas
+
+pconn = ibm_db_dbi.Connection(conn)
+
+selectQuery = "select * from INSTRUCTOR"
+pdf = pandas.read_sql(selectQuery, pconn)
+print(f'Pandas read: \n{pdf}')
+print(pdf.shape)
 
 ibm_db.close(conn)
